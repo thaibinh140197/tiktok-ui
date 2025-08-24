240f5dff2fa36677030e3a5dbd317fb9ee17229d
thaibinh140197 <thaibinh140197@gmail.com>
2025-08-24 22:36:27 +0700

    Bài 62: Cấu hình Router/Layout cho dự án

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+const { override, useBabelRc, addWebpackAlias } = require("customize-cra");
+const path = require('path');
+
+module.exports = override(
+    useBabelRc()
+    , addWebpackAlias({ '~': path.resolve(__dirname, 'src') })
+);
